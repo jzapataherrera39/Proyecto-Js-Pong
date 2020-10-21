17528d3b3991c680212ff3cb42ccedb0019b7082
HeydiEstrada <Hestradajaraba84@correo.unicordoba.edu.co>
2020-10-21 08:36:51 -0500

    Api Rest inicio
    Implementación de la ruta raiz del proyecto

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express'); 
+const express = require('express');
 const app = express(); 
 const port = 3000; 
 app.get('/', (req, res) => { 
-  res.send('Bienvido a la creacion de esta api basica!, Este Api fue desarrollada por Jorge Zapata, Veronica Sandon, Heydi estrada, Leonardo berrocal'); 
+  res.send('Bienvenidos a la creación de esta Api básica!, Este Api fue desarrollada por Jorge Zapata, Veronica Sandon, Heydi Estrada, Leonardo berrocal'); 
 }) 
 
 app.listen(port, () => { 
   console.log(`Example app listening at http://localhost:${port}`); 
 }) 
-process.env.RUNKIT_ENDPOINT_URL;
+
 
 
