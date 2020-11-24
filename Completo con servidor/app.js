7f0e22f3f86d95f5b00d98f247314b7cf58de670
jzapataherrera39 <71646939+jzapataherrera39@users.noreply.github.com>
2020-11-24 17:35:37 -0500

    Summary

---
+++ Completo con servidor/app.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var app     = express();
+var http    = require('http').Server(app);
+var io      = require('socket.io')(http);
+var path    = require('path');
+
+app.use(express.static(path.join(__dirname,"public")));
+
+var port = process.env.PORT || 3000;
+http.listen(port, function(){
+  console.log("server on!: http://localhost:3000/");
+});
+
+var SETTINGS = require("./pongOnline/SETTINGS.js");
+
+var lobbyManager = new (require('./pongOnline/LobbyManager.js'))(io);
+var roomManager = new (require('./pongOnline/RoomManager.js'))(io);
+var gameManager = new (require('./pongOnline/GameManager.js'))(io, roomManager);
+
+io.on('connection', function(socket){
+  console.log('user connected: ', socket.id);
+//  console.log('$  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $  $\n', socket);
+
+  lobbyManager.push(socket);
+  lobbyManager.dispatch(roomManager);
+
+  io.to(socket.id).emit('connected', SETTINGS);
+
+  socket.on('disconnect', function(){
+    var roomIndex = roomManager.roomIndex[socket.id];
+    if(roomIndex){
+      roomManager.destroy(roomIndex, lobbyManager);
+    }
+    lobbyManager.kick(socket);
+    lobbyManager.dispatch(roomManager);
+    console.log('user disconnected: ', socket.id);
+    //console.log(socket);
+  });
+  socket.on('keydown', function(keyCode){
+    var roomIndex = roomManager.roomIndex[socket.id];
+    if(roomIndex){
+      roomManager.rooms[roomIndex].objects[socket.id].keypress[keyCode] = true;
+    }
+  });
+  socket.on('keyup', function(keyCode){
+    var roomIndex = roomManager.roomIndex[socket.id];
+    if(roomIndex)
+      delete roomManager.rooms[roomIndex].objects[socket.id].keypress[keyCode];
+  });
+});
---
+++ Completo con servidor/pongOnline/SETTINGS.js
@@ -0,0 +1,20 @@
+var SETTINGS = {
+  WIDTH : 600,
+  HEIGHT : 400,
+  BORDER_WIDTH : 10,
+  BACKGROUND_COLOR : "#FFFFFF",
+  PLAYER : {
+    WIDTH : 15,
+    HEIGHT : 80,
+    GAP: 20
+  },
+  BALL : {
+    WIDTH : 15,
+    HEIGHT : 15,
+  },
+  NET : {
+    WIDTH : 6
+  }
+};
+
+module.exports = SETTINGS;
